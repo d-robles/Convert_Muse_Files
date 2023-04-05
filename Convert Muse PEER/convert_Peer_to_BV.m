c7f6b2a2703825a2b4d21868943f564b3bb9ea04
Daniel Robles <38795068+d-robles@users.noreply.github.com>
2023-04-05 09:28:33 -0400

    Update convert_Peer_to_BV.m

--- Convert Muse PEER/convert_Peer_to_BV.m
+++ Convert Muse PEER/convert_Peer_to_BV.m
@@ -7,6 +7,8 @@
 %%%UPDATE HISTORY%%%
 %July 2017 - Created for the app Peer, converted from the script
         %'convert_MUSE_to_BV_2_0'
+        
+%adapted by Daniel Robles to convert Malawi Muse files to github. April 2023. Edits made by Daniel Robles are signed as DR.    
 
 %% Starting information
 clear all; close all; clc; %Clear data
@@ -14,7 +16,7 @@ clear all; close all; clc; %Clear data
 %Directories and pathways
 working_dir = uigetdir('','Find the folder with your data'); %Point to where the data is
 cd(working_dir); %Change directory to where the data is
-addpath(fullfile(fileparts(which('convert_MUSE_to_BV.m')),'./Other Files')); %Add the path for supporting files
+addpath(fullfile(fileparts(which('convert_MUSE_to_BV.m')),'./Other Files')); %Add the path for supporting files %the 
 
 %New data folder check and creation
 dircheck = exist(fullfile(working_dir, 'BV Data')); %Check to see if BV Data folder exists
@@ -23,7 +25,7 @@ if dircheck ~= 7
 end
 
 %% Finding files
-prefix = cell2mat(inputdlg('Enter the prefix of your participant filename','Peer to BV',1,{'Participant_'})); %Input participant file name
+prefix = cell2mat(inputdlg('Enter the prefix of your participant filename','Peer to BV',1,{'Participant_'})); %Input participant file name %For malawi data, use 'eeg_' as the prefix - DR
 filenames = dir(strcat(prefix,'*')); %Create matrix of file names in the directory with the prefix
 
 %% Process Data
@@ -46,10 +48,10 @@ for counter = 1:length(filenames) %Number of participants - if this is incorrect
     markers = markers(:,markerindex); %Keep only non-zero markers
     
     %The structure EEG will be used to convert into BV
-    [EEG.data] = csvdata(:,2:5)'; %Create variable for EEG data
+    [EEG.data] = csvdata(:,2:6)'; %Create variable for EEG data %changed to 6 to include aux channel. DR. 
     [EEG.srate] = 256; %The sampling rate
     [EEG.chanlocs] = readlocs('MUSE.locs'); %The electrodes and their locations (requires EEGLab Matlab Toolbox)
-    [EEG.nbchan] = 4; %Number of electrodes
+    [EEG.nbchan] = 5; %Number of electrodes %changed to 5 to include Pz - DR. 
     [EEG.trials] = length(markers); %Total number of trials
     for markcount = 1:length(markers)
         [EEG.event(markcount).type] = ['S ' num2str(markers(1,markcount))]; %Insert marker names
